63eec42a0b3b21cef0d1bebe12f0979847084a74
Peter H. Simonsen <peterhusted@hotmail.com>
2018-12-11 14:39:44 +0100

    Update timeConverter.py

--- timeConverter.py
+++ timeConverter.py
@@ -1,10 +1,21 @@
 import time
 
 # while True:
-c = time.gmtime()
+
+t = time.time() + 60*60
+c = time.gmtime(t)
 print(c)
-c.tm = c.tm_hour 
 
 
-x = time.strftime("%a, %d %b %Y %H:%M:%S CET", time.gmtime())
+x = time.strftime("%a, %d %b %Y %H:%M:%S CET", c)
 print(x)
+
+now = time.strftime("%H:%M:%S", c)
+print(now)
+
+
+acttime = time.asctime(time.localtime())
+print(acttime)
+acttime = acttime.split()
+print(acttime)
+
