a1b19f1466952e652fc1ae8e5474f85ce4d58f1e
Peter H. Simonsen <peterhusted@hotmail.com>
2018-12-27 14:18:41 +0100

    Update timeConverter.py

--- timeConverter.py
+++ timeConverter.py
@@ -1,5 +1,5 @@
 import time
-
+import tkinter as tk
 # while True:
 
 t = time.time() + 60*60
@@ -19,3 +19,17 @@ print(acttime)
 acttime = acttime.split()
 print(acttime)
 
+tkWindow = tk.Tk()
+tkWindow.geometry("250x170")
+tkWindow.title("Time")
+
+tfloor1 = tk.Label(tkWindow,text=now)
+
+tfloor1.grid(column=1, row=1)
+
+while True:
+    t = time.time() + 60*60
+    c = time.gmtime(t)
+    now = time.strftime("%H:%M:%S", c)
+    tfloor1 = tk.Label(tkWindow,text=now)
+    tk.mainloop()
